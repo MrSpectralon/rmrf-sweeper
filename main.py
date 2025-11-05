98c265c45c9723abfb800b7b8a09c70d4ab78d13
Stian <Nuuh@noscrape.fuckoff>
2025-11-05 17:49:38 +0000

    Lagde en litt simplere versjon med et brukbart eksempel

--- main.py
+++ main.py
@@ -1,77 +1,26 @@
 import tkinter as tk
 
+def print_entered_value(e):
+    value = e.get()
+    print("You entered :", value)
+
+def make_text_field(window, lable_text, default_value) ->tk.Entry:
+    label = tk.Label(window, text=lable_text)
+    label.pack()
+    entry = tk.Entry(conf_window)
+    entry.insert(0, default_value)
+    entry.pack()
+    return entry
 
 
-def monthly_payment(entries):
-    # period rate:
-    r = (float(entries['Annual Rate'].get()) / 100) / 12
-    print("r", r)
-    # principal loan:
-    loan = float(entries['Loan Principle'].get())
-    n =  float(entries['Number of Payments'].get())
-    remaining_loan = float(entries['Remaining Loan'].get())
-    q = (1 + r)** n
-    monthly = r * ( (q * loan - remaining_loan) / ( q - 1 ))
-    monthly = ("%8.2f" % monthly).strip()
-    entries['Monthly Payment'].delete(0, tk.END)
-    entries['Monthly Payment'].insert(0, monthly )
-    print("Monthly Payment: %f" % float(monthly))
+if __name__ == "__main__":
+    conf_window = tk.Tk()
+    conf_window.title("PythonExamples.org")
+    conf_window.geometry("300x200")
+    size_x = make_text_field(conf_window, "Size X: ", "1337")
+    button = tk.Button(conf_window, text="Submit", command=lambda: print_entered_value(size_x))
+    button.pack(side=tk.LEFT)
+    exit_button = tk.Button(conf_window, text="EXIT", command=conf_window.quit)
+    exit_button.pack(side=tk.RIGHT)
 
-    
-def final_balance(entries):
-    r = (float(entries['Annual Rate'].get()) / 100) / 12
-    print("r", r)
-    # principal loan:
-    loan = float(entries['Loan Principle'].get())
-    n =  float(entries['Number of Payments'].get()) 
-    monthly = float(entries['Monthly Payment'].get())
-    q = (1 + r) ** n
-    remaining = q * loan  - ( (q - 1) / r) * monthly
-    remaining = ("%8.2f" % remaining).strip()
-    entries['Remaining Loan'].delete(0, tk.END)
-    entries['Remaining Loan'].insert(0, remaining )
-    print("Remaining Loan: %f" % float(remaining))
-
-def init(root):
-    fields = ('Size X', 'Size Y', '% bombs (5-99)')
-   
-    # det er lager size x
-    SizeX = tk.Frame(root)
-    lab = tk.Label(SizeX, width=22, text="Size X: ", anchor='w')
-    ent = tk.Entry(SizeX)
-    ent.insert(0, "5")
-    SizeX.pack(side=tk.TOP,fill=tk.X, padx=5, pady=5)
-    lab.pack(side=tk.LEFT)
-    ent.pack(side=tk.RIGHT,expand=tk.YES, fill=tk.X)
-    
-    SizeY = tk.Frame(root)
-    lab = tk.Label(SizeY, width=22, text="Size Y: ", anchor='w')
-    ent = tk.Entry(SizeY)
-    ent.insert(0, "5")
-    SizeY.pack(side=tk.TOP,fill=tk.X, padx=5, pady=5)
-    lab.pack(side=tk.LEFT)
-    ent.pack(side=tk.RIGHT,expand=tk.YES, fill=tk.X)
-    
-    difficulty = tk.Frame(root)
-    lab = tk.Label(difficulty, width=22, text="% bombs (5-99): ", anchor='w')
-    ent = tk.Entry(difficulty)
-    ent.insert(0, "10")
-    difficulty.pack(side=tk.TOP,fill=tk.X, padx=5, pady=5)
-    lab.pack(side=tk.LEFT)
-    ent.pack(side=tk.RIGHT,expand=tk.YES, fill=tk.X)
-        
-    return SizeX, SizeY, difficulty
-
-if __name__ == '__main__':
-    root = tk.Tk()
-    root.title("Minesweeper")
-    ents = init(root)
-    b1 = tk.Button(root, text='Final Balance',
-           command=(lambda e=ents: final_balance(e)))
-    b1.pack(side=tk.LEFT, padx=5, pady=5)
-    b2 = tk.Button(root, text='Monthly Payment',
-           command=(lambda e=ents: monthly_payment(e)))
-    b2.pack(side=tk.LEFT, padx=5, pady=5)
-    b3 = tk.Button(root, text='Quit', command=root.quit)
-    b3.pack(side=tk.LEFT, padx=5, pady=5)
-    root.mainloop()
+    conf_window.mainloop()
